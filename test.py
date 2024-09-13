c9c807c8596a09013989c6e559f123e0d99df9e8
Davi Belo <davibelo@gmail.com>
2024-09-13 16:22:58 -0300

    good test!

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import win32com.client as win32
 
 # 1. Specify file name
-file = 'RECAP_revJ.bkp'
+file = r"RECAP_revJ\RECAP_revJ.bkp"
 
 # 2. Get path to Aspen Plus file
 aspen_Path = os.path.abspath(file)
@@ -16,18 +16,21 @@ print('Connected!')
 Application.InitFromArchive2(aspen_Path)
 
 # 5. Make the files visible
-Application.visible = 1
+Application.visible = 0
 
-# Example: Reading input and output variables
+
 test_temp = Application.Tree.FindNode(r"\Data\Streams\G-ACID\Output\TEMP_OUT\MIXED").Value
 print(f"temperature: {test_temp} °C")
 
-# Example: Reading output after simulation
 test_mass_frac = Application.Tree.FindNode(r"\Data\Streams\AGUAPR1\Output\MASSFRAC\MIXED\H2S").Value
-print(f"Mass Fraction H2S: {test_mass_frac}")
+print(f"Mass Fraction H2S before: {test_mass_frac}")
 
-# Example: Modifying a variable
-Application.Tree.FindNode(r"\Data\Blocks\N-640\Input\QN").Value = 565000
+test_reboiler_duty = Application.Tree.FindNode(r"\Data\Blocks\N-640\Input\QN").Value
+print(f"Reboiler Duty before: {test_reboiler_duty}")
+
+new_reboiler_duty = test_reboiler_duty + 10000
+print(f"Modifying Reboiler Duty to {new_reboiler_duty}")
+Application.Tree.FindNode(r"\Data\Blocks\N-640\Input\QN").Value = new_reboiler_duty
 
 # Run the simulation after changing input variables
 Application.Engine.Run2()
