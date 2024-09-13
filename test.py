31092196aa5462eb1e2b4c5030d31de046065fb7
Davi Belo <davibelo@gmail.com>
2024-09-13 10:08:13 -0300

    first successfull run

---
+++ test.py
@@ -0,0 +1,36 @@
+import os
+import win32com.client as win32
+
+# 1. Specify file name
+file = 'RECAP_revJ.bkp'
+
+# 2. Get path to Aspen Plus file
+aspen_Path = os.path.abspath(file)
+
+# 3 Initiate Aspen Plus application
+print('\n Connecting to the Aspen Plus... Please wait ')
+Application = win32.Dispatch('Apwn.Document') # Registered name of Aspen Plus
+print('Connected!')
+
+# 4. Initiate Aspen Plus file
+Application.InitFromArchive2(aspen_Path)
+
+# 5. Make the files visible
+Application.visible = 1
+
+# Example: Reading input and output variables
+test_temp = Application.Tree.FindNode("\Data\Streams\G-ACID\Output\TEMP_OUT\MIXED").Value
+print(f"temperature: {test_temp} °C")
+
+# # Example: Modifying a variable
+# Application.Tree.FindNode(r'\Data\Blocks\N-640\Input\QR').Value = 565000
+
+# # Run the simulation after changing input variables
+# Application.Engine.Run2()
+
+# # Example: Reading output after simulation
+# mass_frac = Application.Tree.FindNode(r'\Data\Streams\AGUAPR1\Output\MASSFRAC\MIXED\H2S')
+# print(f"Mass Fraction H2S: {mass_frac}")
+
+# Close the Aspen simulation
+Application.Close()
