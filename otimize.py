77fcc429a33ec8b7006d33a9105a8e3a8a767902
Davi Belo <davibelo@gmail.com>
2024-09-13 21:57:56 -0300

    otimize boiler plate

--- otimize.py
+++ otimize.py
@@ -10,3 +10,30 @@ print('Connected!')
 
 Application.InitFromArchive2(aspen_Path)
 Application.visible = 0
+
+# Objective function to minimize
+def cost(x):
+    N640QN = x
+    return N640QN
+
+# Constraint 1
+def constraint1(x):
+    cH2S = x - 0.00010
+    return cH2S
+
+# Initial guess
+x0 = [550000]
+
+# Constraints as a dictionary
+constraints = [{'type': 'ineq', 'fun': constraint1}]
+
+# Bounds
+bounds = [(520000, 600000)]
+
+# Solving the optimization problem
+result = minimize(cost, x0, method='SLSQP', bounds=bounds, constraints=constraints)
+
+# Output results
+N640QN_opt = result.x
+cost_min = result.fun
+print(N640QN_opt, cost_min)
