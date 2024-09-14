fd2c8e77e752725da53fd733debe325a110bf19c
Davi Belo <davibelo@gmail.com>
2024-09-13 21:46:20 -0300

    test

---
+++ otimize.py
@@ -0,0 +1,12 @@
+import os
+import win32com.client as win32
+
+file = r"RECAP_revJ\RECAP_revJ.bkp"
+aspen_Path = os.path.abspath(file)
+
+print('Connecting to the Aspen Plus... Please wait ')
+Application = win32.Dispatch('Apwn.Document') # Registered name of Aspen Plus
+print('Connected!')
+
+Application.InitFromArchive2(aspen_Path)
+Application.visible = 0
